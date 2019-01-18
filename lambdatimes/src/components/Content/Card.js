a0cd58a89269408824325eb922d23583acb4e905
Brandon Lent <blent1050@yahoo.com>
2019-01-18 13:04:09 -0500

    Refactored code to get rid of unused variable warnings

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -2,13 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Card = props => {
-  console.log(props)
   return (
     <div className="card">
       <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.card.img} />
+          <img src={props.card.img} alt={`Published by ${props.card.author}`} />
         </div>
         <span>By {props.card.author}</span>
       </div>
